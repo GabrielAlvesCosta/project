f1c981d1e522bcb246511ea82fb923397c687e15
gabrielalvescosta <gabriel.a.costa23@aluno.senai.br>
2026-04-13 17:17:28 -0300

    a

--- database.sql
+++ database.sql
@@ -1,5 +1,7 @@
 create database database1;
 
+use database1;
+
 CREATE TABLE usuarios (
    id INT AUTO_INCREMENT PRIMARY KEY,
    email VARCHAR(100) UNIQUE,
@@ -8,3 +10,8 @@ CREATE TABLE usuarios (
    tentativas_login INT DEFAULT 0,
    ultimo_login DATETIME
 );
+
+insert into usuarios (email, senha, ativo)
+values ('usuario@teste.com', senha, TRUE);
+
+select * from usuarios;
