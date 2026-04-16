01617b4141bcb4226636890eaaa6203deaa305a5
gabrielalvescosta <gabriel.a.costa23@aluno.senai.br>
2026-04-16 08:54:26 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -17,9 +17,6 @@ insert into usuarios (email, senha, ativo)
 values ('usuario@teste.com', senha, TRUE);
 
 select * from usuarios;
-
-UPDATE usuarios SET senha = 'scrypt:32768:8:1$gm7CLEYhLxpfMfcP$09ff4fd9507b5afccab3e45ad73dd08d3a72ea0853358f1cd4a14586f324c768bf4de51c2ca511b64b237422aa3adb4a6ceee81b9087f9f393129d033fa31df7'
- WHERE senha = '0' OR senha IS NULL;
  
  UPDATE usuarios SET bloqueado_ate = NUll
  WHERE tentativas_login = '3' OR tentativas_login = '4';
