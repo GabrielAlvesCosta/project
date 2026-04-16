d45956738765d675278dd02c46261c8be2abc56d
gabrielalvescosta <gabriel.a.costa23@aluno.senai.br>
2026-04-16 08:52:12 -0300

    aaaaaaaaaaaa

--- database.sql
+++ database.sql
@@ -11,12 +11,12 @@ CREATE TABLE usuarios (
    ultimo_login DATETIME
 );
 
+ALTER TABLE usuarios ADD COLUMN bloqueado_ate DATETIME NULL;
+
 insert into usuarios (email, senha, ativo)
 values ('usuario@teste.com', senha, TRUE);
 
 select * from usuarios;
-
-ALTER TABLE usuarios ADD COLUMN bloqueado_ate DATETIME NULL;
 
 UPDATE usuarios SET senha = 'scrypt:32768:8:1$gm7CLEYhLxpfMfcP$09ff4fd9507b5afccab3e45ad73dd08d3a72ea0853358f1cd4a14586f324c768bf4de51c2ca511b64b237422aa3adb4a6ceee81b9087f9f393129d033fa31df7'
  WHERE senha = '0' OR senha IS NULL;
@@ -27,11 +27,12 @@ UPDATE usuarios SET senha = 'scrypt:32768:8:1$gm7CLEYhLxpfMfcP$09ff4fd9507b5afcc
 UPDATE usuarios SET tentativas_login = 0
 WHERE tentativas_login = '3' OR tentativas_login = '4';
  
-UPDATE usuarios SET senha = NULL
-WHERE email = 'gollumalvescosta@gmail.com';
-
 UPDATE usuarios SET ultimo_login = NULL
-WHERE email = 'usuario@teste.com';
+WHERE id = '1' or id = '2' or id = '3' or id = '4' or id = '8';
 
+UPDATE usuarios SET senha = NULL
+WHERE id = '17' or id = '23' or id = '29' or id = '30';
 
+DELETE FROM usuarios 
+WHERE id = 8;
  
